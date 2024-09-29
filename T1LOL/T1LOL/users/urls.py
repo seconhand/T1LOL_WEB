f50a8b5196cca00a2ef9f6aba82d445c6e2cb4ee
seconhand <gkscldbs123@naver.com>
2024-09-29 19:27:44 +0900

    1stday login_out

---
+++ T1LOL/T1LOL/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+#회원가입, 로그인, 로그아웃, 비밀번호 재설정 URL 설정
+urlpatterns  = [
+    path('signup/', views.signup, name = 'signup'),
+    path('login/', auth_views.LoginView, name = 'login'),
+    path('logout/', auth_views.LogoutView, name = 'logout'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name = 'password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name = 'password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
+]
